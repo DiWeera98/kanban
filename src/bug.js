6fff8dd76e140346a2fe5e5401712d175a1c03e2
Dinithi Weerakoon <dinithisandarekha@gmail.com>
2021-03-16 22:45:39 +0530

    Card Styling

--- src/bug.js
+++ src/bug.js
@@ -1,12 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 import { Draggable } from "react-beautiful-dnd";
+import { Badge, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { BsClock, BsExclamationTriangle } from "react-icons/bs";
 
 const Container = styled.div`
   border: 1px solid lightgrey;
   border-radius: 2px;
   padding: 8px;
   margin-bottom: 8px;
+  box-shadow: 1px 1px rgba(0, 0, 0, 0.1);
   background-color: ${(props) =>
     props.isDragDisabled
       ? "lightgrey"
@@ -16,25 +19,59 @@ const Container = styled.div`
 `;
 
 const Bug = (props) => {
-  const isDragDisabled = props.task.id === "task-1";
   return (
-    <Draggable
-      draggableId={props.task.id}
-      index={props.index}
-      isDragDisabled={isDragDisabled}
-    >
-      {(provided, snapshot) => (
-        <Container
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-          ref={provided.innerRef}
-          isDragging={snapshot.isDragging}
-          isDragDisabled={isDragDisabled}
-        >
-          {props.task.content}
-        </Container>
-      )}
-    </Draggable>
+    <div style={{}}>
+      <Draggable draggableId={props.task.id} index={props.index}>
+        {(provided, snapshot) => (
+          <Container
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            ref={provided.innerRef}
+            isDragging={snapshot.isDragging}
+          >
+            {props.task.content}
+            <br />
+            <OverlayTrigger overlay={<Tooltip>Priority</Tooltip>}>
+              <Badge
+                pill
+                variant={
+                  props.task.priority === "Low"
+                    ? "primary"
+                    : props.task.priority === "Medium"
+                    ? "success"
+                    : props.task.priority === "High"
+                    ? "warning"
+                    : "danger"
+                }
+              >
+                <BsClock />
+              </Badge>
+            </OverlayTrigger>
+            &nbsp;
+            <OverlayTrigger overlay={<Tooltip>Severity</Tooltip>}>
+              <Badge
+                pill
+                variant={
+                  props.task.priority === "Low"
+                    ? "primary"
+                    : props.task.priority === "Medium"
+                    ? "success"
+                    : props.task.priority === "High"
+                    ? "warning"
+                    : "danger"
+                }
+              >
+                <BsExclamationTriangle />
+              </Badge>
+            </OverlayTrigger>
+            &nbsp;
+            <OverlayTrigger overlay={<Tooltip>Bug Type</Tooltip>}>
+              <Badge variant="dark">{props.task.bugType}</Badge>
+            </OverlayTrigger>
+          </Container>
+        )}
+      </Draggable>
+    </div>
   );
 };
 
